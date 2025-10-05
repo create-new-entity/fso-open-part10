a364186c64d9ff4fccba484c5751016e1167ae18
Pavel <imran.pavel@gmail.com>
2025-10-05 17:44:27 +0300

    10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,67 @@
 
+import { useFormik } from 'formik';
+import { TextInput, Button, StyleSheet, View } from 'react-native';
 
-import Text from './Text';
+
+const styles = StyleSheet.create({
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: 20,
+    marginTop: 20,
+    padding: 10,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 5,
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textInputStyle: {
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 5,
+    width: '95%'
+  }
+});
+
+const initialValues = {
+  username: '',
+  password: ''
+};
+
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const formik = useFormik({
+    initialValues,
+    onSubmit: (values) => {
+      console.log(values)
+      formik.handleReset()
+    }
+  });
+  return (
+    <View style={styles.formContainer}>
+      <TextInput
+        style={styles.textInputStyle}
+        placeholder='username'
+        value={formik.values.username}
+        onChange={formik.handleChange('username')}
+      />
+      <TextInput
+        style={styles.textInputStyle}
+        placeholder='password'
+        secureTextEntry
+        value={formik.values.password}
+        onChange={formik.handleChange('password')}
+      />
+      <Button
+        title='Sign In'
+        onPress={formik.handleSubmit}
+      />
+    </View>
+  );
 };
 
 export default SignIn;
