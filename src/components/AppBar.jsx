0ee666a68e84d942e4b0845aaebb371d50409d99
Pavel <imran.pavel@gmail.com>
2025-10-05 17:14:29 +0300

    10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView, Dimensions, Text } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
@@ -8,11 +8,12 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     height: 50,
+    width: Dimensions.get('window').width,
     backgroundColor: theme.colors.appBar,
     display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-end',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
     gap: 5,
     paddingLeft: 5,
     paddingBottom: 5
@@ -22,6 +23,10 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'flex-start',
     gap: 5
+  },
+  scrollView: {
+    maxHeight: 'fit-content',
+    width: Dimensions.get('window').width
   }
 });
 
@@ -29,17 +34,22 @@ const AppBar = () => {
   
   return (
     <View style={styles.container}>
-        <AppBarTab
-            text={'Repositories'}
-            textStyles={styles.containerText}
-            linkTo={'/'}
-        />
-        <AppBarTab
-            text={'Sign In'}
-            textStyles={styles.containerText}
-            linkTo={'/sign_in'}
-        />
+        <ScrollView horizontal style={styles.scrollView}>
+            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', gap: 8}}>
+                <AppBarTab
+                    text={'Repositories'}
+                    textStyles={styles.containerText}
+                    linkTo={'/'}
+                />
+                <AppBarTab
+                    text={'Sign In'}
+                    textStyles={styles.containerText}
+                    linkTo={'/sign_in'}
+                />
+            </View>
+        </ScrollView>
     </View>
+   
   );
 };
 
