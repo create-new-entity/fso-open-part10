58f2bf21c70a8260d8f7116490ded6c6d4815349
Pavel <imran.pavel@gmail.com>
2025-10-05 18:14:41 +0300

    10.10

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,4 @@
+import { Platform } from "react-native";
 
 
 const theme = {
@@ -14,7 +15,11 @@ const theme = {
     subheading: 16,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      ios: 'Arial',
+      android: 'Roboto',
+      default: 'System'
+    }),
   },
   fontWeights: {
     normal: '400',
