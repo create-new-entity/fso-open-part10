a23fa2d29abbfd9f0e34a7e37582f58a9b7d5733
Pavel <imran.pavel@gmail.com>
2025-10-06 14:11:37 +0300

    Follow up refactorings

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,7 +1,10 @@
 import { useMutation } from "@apollo/client/react";
 import { SIGN_IN } from "../graphql/mutation";
 
+import useAuthStorage from "./useAuthStorage";
+
 export const useSignIn = () => {
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(SIGN_IN);
 
   const signIn = async ({ username, password }) => {
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,11 +1,30 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import Constants from 'expo-constants';
+import { setContext } from '@apollo/client/link/context';
 
-const createApolloClient = () => {
+const httpLink = new HttpLink({
+  uri: Constants.expoConfig.extra.APOLLO_URI
+});
+
+const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        headers,
+      };
+    }
+  });
   return new ApolloClient({
-    link: new HttpLink({
-      uri: Constants.expoConfig.extra.APOLLO_URI
-    }),
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 };
