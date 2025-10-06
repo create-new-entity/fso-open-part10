219c2ab7fbef8d1efe0ce5946104dc9f73e032d0
Pavel <imran.pavel@gmail.com>
2025-10-06 16:00:54 +0300

    10.15

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,16 +1,29 @@
-import { useMutation } from "@apollo/client/react";
+import { useApolloClient, useMutation } from "@apollo/client/react";
 import { SIGN_IN } from "../graphql/mutation";
 
 import useAuthStorage from "./useAuthStorage";
+import { useNavigate } from "react-router-native";
 
 export const useSignIn = () => {
   const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(SIGN_IN);
+  const apolloClient = useApolloClient();
+  const navigate = useNavigate();
+
+
 
   const signIn = async ({ username, password }) => {
-    return await mutate({
+    const result = await mutate({
         variables: { username, password }
     })
+
+    const { data: { authenticate : { accessToken }} } = result
+
+    await authStorage.setAccessToken(accessToken);
+    apolloClient.resetStore();
+    navigate('/');
+
+    return result;
   };
 
   return [signIn, result];
